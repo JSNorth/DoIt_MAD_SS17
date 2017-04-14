b895201e7a16ceb7ea02514e2396f59e17c1f609
JSNorth <j.s.neurath@googlemail.com>
2017-04-14 14:49:12 +0200

    Anlegen der ToDos lokal

---
+++ app/src/main/java/de/fh_luebeck/jsn/doit/fragments/DatePicker.java
@@ -0,0 +1,11 @@
+package de.fh_luebeck.jsn.doit.fragments;
+
+import DialogFragment;
+import DatePickerDialog.OnDateSetListener;
+
+/**
+ * Created by USER on 14.04.2017.
+ */
+
+public class DatePicker extends DialogFragment implements OnDateSetListener {
+}
